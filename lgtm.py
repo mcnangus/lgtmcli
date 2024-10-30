4f6cc5fa346d01342c5f95b3b358edfe26491163
Angus McNamara <angus.mcnamara@versent.com.au>
2024-10-30 11:53:41 +1100

    feat: add edit flag, remove duplication

--- lgtm.py
+++ lgtm.py
@@ -4,103 +4,148 @@ import argparse
 import json
 import subprocess
 
-GITHUB_HEADERS=['-H', "Accept: application/vnd.github+json", '-H', "X-GitHub-Api-Version: 2022-11-28"]
 
-def gh_api(path: str, *args):
-    return subprocess.check_output([
-        'gh', 'api', *GITHUB_HEADERS,
-        path,
-        *args,
-    ]).decode('utf-8').strip()
+class GHApi:
+    headers=['-H', "Accept: application/vnd.github+json", '-H', "X-GitHub-Api-Version: 2022-11-28"]
+
+    def __init__(self):
+        # TODO load in bash script???
+        remote=subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode('utf-8').strip()
+        baseurl=subprocess.check_output(['dirname', remote]).decode('utf-8').strip()
+        if 'git@github.com' in remote:
+            self.org=baseurl.split(':')[1]
+        else:
+            self.org=subprocess.check_output(['basename', baseurl]).decode('utf-8').strip()
+        self.repo=subprocess.check_output(['basename', '-s', '.git', remote]).decode('utf-8').strip()
+
+    def gh_api(self, path: str, method='GET', *args):
+        return subprocess.check_output([
+            'gh', 'api', '--method', method,
+            *self.headers,
+            f'/repos/{self.org}/{self.repo}/'+path,
+            *args,
+        ]).decode('utf-8').strip()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Open a pull request for editing comments')
-    parser.add_argument('--pr', type=int, help='The number of the pull request to open')
-    parser.add_argument('--file', type=str, help='(optional) The file path to edit (e.g., path/to/file.md)')
-    parser.add_argument('--line', type=str, help='(optional) The starting line that you wish to leave the comment on')
-    parser.add_argument('--comment', type=str, help='Rich text comment')
-    parser.add_argument('--view', default=False, help='View only mode', action=argparse.BooleanOptionalAction)
+    # parse the arguments
+    parser = argparse.ArgumentParser(description='Tool for viewing/editing/creating pull request comments')
+    parser.add_argument('-p', '--pr', type=int, help='The number of the pull request to open')
+    parser.add_argument('-F', '--file', type=str, help='(optional) The path to a specific file for action (e.g., path/to/file.md)')
+    parser.add_argument('-l', '--line', type=str, help='(optional) Starting line of file for action')
+
+    # TODO convert these flags into operations???
+    parser.add_argument('-c', '--comment', type=str, help='Action: Rich text comment')
+    parser.add_argument('-v', '--view', default=False, help='Action: View only mode', action=argparse.BooleanOptionalAction)
+    parser.add_argument('-e', '--edit', default=False, help='Action: Edit mode', action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
 
+
+    # make sure pr is set
     if args.pr == None:
         # TODO use branch to get any available prs
         raise Exception("Error: No pr number provided.")
+    else:
+        try:
+            subprocess.check_output(['gh', 'pr', 'view', str(args.pr)])
+        except:
+            return
 
+
+    # ensure one of view, edit, or comment is set
     if args.view:
         if args.comment != None:
             raise Exception('Cannot set both view and comment.')
+        if args.edit:
+            raise Exception('Cannot set both view and edit modes.')
         print('Entered view only mode...')
+    elif args.edit:
+        if args.comment != None:
+            raise Exception('Cannot set both edit and comment.')
+        print('Entered edit mode...')
     elif args.comment == None:
-        raise Exception("Error: No comment provided.")
+        raise Exception("Error: No mode (view/edit/comment) provided.")
 
-    # TODO load in bash script???
-    remote=subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode('utf-8').strip()
-    baseurl=subprocess.check_output(['dirname', remote]).decode('utf-8').strip()
-    org=subprocess.check_output(['basename', baseurl]).decode('utf-8').strip()
-    repo=subprocess.check_output(['basename', '-s', '.git', remote]).decode('utf-8').strip()
+
+    # load the org, repo, head
+    api = GHApi()
+    # TODO this is the wrong HEAD for the pull request if not on the branch
+    # (options) auto switch to correct branch (and return back afterwards)
+    # (options) get the head from gh cli calls if not in the correct branch
+    # (options) always use the lookup latest commit on the branch the the pull request is on
     head=subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
 
-    # pull request level comments (no threads)
+
+    # pull request level comments (no threads here)
     if args.file == None:
         if args.line != None:
-            raise Exception("Error: No file path provided but line number provided.")
+            raise Exception("No file path provided but line number provided, exiting...")
 
         # TODO handle selecting from existing pr level comments and then editing (stretch), fzf???
-        pr_comments = json.loads(gh_api(f'/repos/{org}/{repo}/pulls/{args.pr}/comments'))
+        pr_comments = json.loads(api.gh_api(f'pulls/{args.pr}/comments'))
 
+        # TODO filter out non-pr level comments, eg. comments on a specific file
         pr_comments = [comment for comment in pr_comments if 'in_reply_to_id' not in comment]
 
-        for comment in pr_comments:
-            print(comment['body'])
-
-        latest_comment=pr_comments[-1]
-        latest_comment_id = latest_comment.get('id', None)
+        if args.view:
+            for comment in pr_comments:
+                print(comment['body'])
+            return
 
         if pr_comments:
+            latest_comment=pr_comments[-1]
+            latest_comment_id = latest_comment.get('id', None)
             print("Existing comment found for this pull request.")
             old_body = latest_comment.get('body', None)
             print(old_body)
-            if not args.view:
-                if input("Do you wish to edit the existing comment? (Y/n): ") in ['Y', 'y']:
-                    new_body = input("Press Enter when you're done editing... ")
-                    if new_body == '':
-                        raise Exception("Empty comment, exiting...")
 
-                    if old_body == new_body:
-                        raise Exception("No changes to comment, exiting...")
+            if args.edit:
+                # TODO make this editable using vim, nano, or other
+                new_body = input("Press Enter when you're done editing... ")
+                if new_body == '':
+                    raise Exception("Empty comment, exiting...")
 
-                    gh_api(
-                        f'/repos/{org}/{repo}/pulls/comments/{latest_comment_id}',
-                        '--method', 'PATCH',
-                        '-f', f"body={new_body}",
-                    )
-                    return
-            else:
+                if old_body == new_body:
+                    raise Exception("No changes, exiting...")
+
+                api.gh_api(f'pulls/comments/{latest_comment_id}', 'PATCH', '-f', f"body={new_body}")
                 return
+        else:
+            if args.edit:
+                raise Exception('No comment to edit, exiting...')
 
-        if not args.view:
-            print("No comments found for this pull request, using comment from input...")
-            subprocess.check_output(['gh', 'pr', 'comment', str(args.pr), '--body', args.comment]).decode('utf-8').strip()
+        print("Using comment from input...")
+        subprocess.check_output(['gh', 'pr', 'comment', str(args.pr), '--body', args.comment]).decode('utf-8').strip()
         return
 
     # TODO resolve file / file and line level comments???
 
+    pr_file_comments = json.loads(api.gh_api(f'pulls/{args.pr}/comments'))
+    pr_file_comments = [comment for comment in pr_file_comments if comment['path'] == args.file]
     if args.line == None:
         # pull request file level comments
-        pr_file_comments = json.loads(gh_api(f'/repos/{org}/{repo}/pulls/{args.pr}/comments'))
-
-        pr_file_comments = [comment for comment in pr_file_comments if comment['path'] == args.file]
-
         latest_comment = None
         for comment in pr_file_comments:
             if 'in_reply_to_id' not in comment:
                 latest_comment = comment
+                if args.view:
+                    print(comment['body'])
+                    comment_thread = [comment for comment in pr_file_comments if 'in_reply_to_id' in comment and comment['in_reply_to_id'] == latest_comment['id']]
+                    for thread_comment in comment_thread:
+                        print('>', thread_comment['body'])
 
-        if latest_comment == None:
-            print('No comments on file', args.file)
-        else:
+
+        if args.view:
+            if latest_comment == None:
+                print('No comments on file', args.file)
+            return
+
+        if args.edit:
+            # TODO implement edit
+            raise Exception('Not implemented yet, exiting.')
+
+        if latest_comment != None:
             latest_comment_id = latest_comment.get('id', None)
             lastest_comment_thread = [comment for comment in pr_file_comments if 'in_reply_to_id' in comment and comment['in_reply_to_id'] == latest_comment['id']]
 
@@ -108,52 +153,83 @@ def main():
             for thread_comment in lastest_comment_thread:
                 print('>', thread_comment['body'])
 
-            if args.view:
-                return
-
             if input("Continue the existing thread? (Y/n): ") in ['Y', 'y']:
-                gh_api(
-                    f'/repos/{org}/{repo}/pulls/{args.pr}/comments/{latest_comment['id']}/replies',
-                    '--method', 'POST',
-                    '-f', f"body={args.comment}",
+                api.gh_api(
+                    f'pulls/{args.pr}/comments/{latest_comment['id']}/replies',
+                    'POST',
+                    '-f' f"body={args.comment}"
                 )
                 return
 
             if input("Create new thread? (Y/n): ") not in ['Y', 'y']:
                 return
 
-        if not args.view:
-            gh_api(
-                f'/repos/{org}/{repo}/pulls/{args.pr}/comments',
-                '--method', 'POST',
-                '-f', f"body={args.comment}",
-                '-f', f"commit_id={head}",
-                '-f', f"path={args.file}",
-                '-f', "side=RIGHT",
-                '-f', "subject_type=file",
-            )
+        api.gh_api(
+            f'pulls/{args.pr}/comments',
+            'POST',
+            '-f', f"body={args.comment}",
+            '-f', f"commit_id={head}",
+            '-f', f"path={args.file}",
+            '-f', "side=RIGHT",
+            '-f', "subject_type=file",
+        )
         return
-
-        # TODO edit existing comment
 
     # TODO multiline, eg. --multiline 5-12 OR --multiline 5:12
     # '-F', "start_line=1", '-f', "start_side=RIGHT"
 
     else:
         # pull request file AND line level comments
-        if not args.view:
-            gh_api(
-                f'/repos/{org}/{repo}/pulls/{args.pr}/comments',
-                '--method', 'POST',
-                '-f', f"body={args.comment}",
-                '-f', f"commit_id={head}",
-                '-f', f"path={args.file}",
-                '-F', f"line={args.line}",
-                '-f', "side=RIGHT",
-            )
+        pr_file_and_line_comments = [comment for comment in pr_file_comments if 'line' in comment and str(comment['line']) == args.line]
+
+        latest_comment = None
+        for comment in pr_file_and_line_comments:
+            if 'in_reply_to_id' not in comment:
+                latest_comment = comment
+                if args.view:
+                    print(comment['body'])
+                    comment_thread = [comment for comment in pr_file_comments if 'in_reply_to_id' in comment and comment['in_reply_to_id'] == latest_comment['id']]
+                    for thread_comment in comment_thread:
+                        print('>', thread_comment['body'])
+
+        if args.view:
+            if latest_comment == None:
+                print('No comments on line', args.line, 'in file', args.file)
+            return
+
+        if args.edit:
+            # TODO implement edit
+            raise Exception('Not implemented yet, exiting.')
+
+        if latest_comment != None:
+            latest_comment_id = latest_comment.get('id', None)
+            lastest_comment_thread = [comment for comment in pr_file_comments if 'in_reply_to_id' in comment and comment['in_reply_to_id'] == latest_comment['id']]
+
+            print(latest_comment['body'])
+            for thread_comment in lastest_comment_thread:
+                print('>', thread_comment['body'])
+
+            if input("Continue the existing thread? (Y/n): ") in ['Y', 'y']:
+                api.gh_api(
+                    f'pulls/{args.pr}/comments/{latest_comment['id']}/replies',
+                    'POST',
+                    '-f' f"body={args.comment}"
+                )
+                return
+
+            if input("Create new thread? (Y/n): ") not in ['Y', 'y']:
+                return
+
+        api.gh_api(
+            f'pulls/{args.pr}/comments',
+            'POST',
+            '-f', f"body={args.comment}",
+            '-f', f"commit_id={head}",
+            '-f', f"path={args.file}",
+            '-F', f"line={args.line}",
+            '-f', "side=RIGHT",
+        )
         return
-        # TODO handle editing existing file/line combo level comments (stretch)
-        # TODO handle replying to thread of file/line combo level comments
 
 
     # TODO interactive editor for commenting
